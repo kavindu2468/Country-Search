3cd8d6e07fa43241770ddce07b09157ed8bd4824
kavindu2468~ <kavindugimshan444@gmail.com>
2024-12-23 14:43:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+let body="";
+fetch('https://restcountries.com/v3.1/all')
+            .then(res=>res.json())
+            .then(data=>{
+                data.forEach(element => {
+                    body+=`<div class="col">
+                    <div class="card shadow-sm">
+                      <img src="${element.flags.png}" alt="">
+                      <div class="card-body">
+                        <h5>${element.name.common}</h5>
+                        <p class="card-text size">Capital:${element.capital}<br><br>Region:${element.region}<br><br>Population:${element.population}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                             <a href="${element.maps.googleMaps}">
+                              <button class="btn btn-primary">View on Google Maps</button>
+                              </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>`
+                });
+                let row=document.getElementById("row");
+                row.innerHTML=body;
+                console.log(data);
+                
+            })
+ let txtcountry=document.getElementById("txtcountry");
+ console.log(txtcountry)
+ txtcountry.addEventListener("input",search);
+function search(){
+    let body="";  
+fetch('https://restcountries.com/v3.1/all')
+            .then(res=>res.json())
+            .then(data=>{
+                data.forEach(element => {
+                    if(element.name.common.includes(txtcountry.value))
+                    body+=`<div class="col">
+                    <div class="card shadow-sm">
+                      <img src="${element.flags.png}" alt="">
+                      <div class="card-body">
+                        <h5>${element.name.common}</h5>
+                        <p class="card-text size">Capital:${element.capital}<br><br>Region:${element.region}<br><br>Population:${element.population}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                             <a href="${element.maps.googleMaps}">
+                              <button class="btn btn-primary">View on Google Maps</button>
+                              </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>`
+                });
+                let row=document.getElementById("row");
+                row.innerHTML=body;
+                console.log(data);
+                
+            })
+
+
+}
+          
+           
